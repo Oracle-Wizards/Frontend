44203d4b80b9b4748e8eca89eaa1fca78bb92c36
HAITAME <haitame.laframe@etu.uae.ac.ma>
2024-02-10 17:12:44 +0100

    init

---
+++ src/pages/playground/playground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+function Playground() {
+  return (
+    <><div>playground</div>
+    
+    <Button>click me</Button></>
+  )
+}
+
+export default Playground

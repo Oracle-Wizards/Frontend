4e233fac0c4e3f23b318edf1d8f2b92cda51e552
SoukainaElidrissi <soukainaedrissi2002@gmail.com>
2024-02-10 21:27:23 +0100

    front

--- src/pages/playground/playground.jsx
+++ src/pages/playground/playground.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from 'react';
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { Textarea } from '@/components/ui/textarea'
+} from "@/components/ui/resizable";
+import { Textarea } from '@/components/ui/textarea';
 
 function Playground() {
   return (
     <>
       <div>playground</div>
       
-
       <div className="bg-white shadow rounded h-screen">
         {/* Resizable panels container */}
         <div className="flex flex-col h-full">
           <ResizablePanelGroup direction="horizontal" className="flex flex-grow">
             <ResizablePanel className="border-r flex-grow">
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">One</span>
-                <Textarea className="mt-4">This is the content of Panel One.</Textarea>
+              <div className="flex flex-col h-full items-center justify-center p-6">
+                <span className="font-semibold">Original query </span>
+                <Textarea className="mt-4 resize-none w-full flex-grow">
+                  Enter your query 
+                </Textarea>
+                {/* Add the Button component here */}
+                <Button>Submit</Button>
               </div>
             </ResizablePanel>
             <ResizableHandle />
@@ -29,13 +40,43 @@ function Playground() {
               <ResizablePanelGroup direction="vertical" className="flex flex-grow">
                 <ResizablePanel className="border-b flex-grow">
                   <div className="flex h-full items-center justify-center p-6">
-                    <span className="font-semibold">Two</span>
+                    <span className="font-semibold">Plan d'execution</span>
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead>bytes</TableHead>
+                          <TableHead>cost</TableHead>
+                          <TableHead>rows</TableHead>
+                          <TableHead>time</TableHead>
+                          <TableHead>name</TableHead>
+                          <TableHead>operation</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>600</TableCell>
+                          <TableCell>2(0)</TableCell>
+                          <TableCell>15</TableCell>
+                          <TableCell>00:00:07</TableCell>
+                          <TableCell>Etudiant</TableCell>
+                          <TableCell>SELECT * FROM Etudiant</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>600</TableCell>
+                          <TableCell>2(0)</TableCell>
+                          <TableCell>15</TableCell>
+                          <TableCell>00:00:07</TableCell>
+                          <TableCell>Etudiant</TableCell>
+                          <TableCell>SELECT * FROM Etudiant</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
                   </div>
                 </ResizablePanel>
                 <ResizableHandle />
                 <ResizablePanel className="flex-grow">
                   <div className="flex h-full items-center justify-center p-6">
-                    <span className="font-semibold">Three</span>
+                    <span className="font-semibold">Optimized Query</span>
                   </div>
                 </ResizablePanel>
               </ResizablePanelGroup>
@@ -47,4 +88,4 @@ function Playground() {
   )
 }
 
-export default Playground
+export default Playground;

e0612e720cf0cc35594734ac4c6ac744fc9338a0
HAITAME <haitame.laframe@etu.uae.ac.ma>
2024-02-11 17:51:26 +0100

    Add eslint-disable for react/no-unescaped-entities

--- src/pages/gemini/gemini.jsx
+++ src/pages/gemini/gemini.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-irregular-whitespace */
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";

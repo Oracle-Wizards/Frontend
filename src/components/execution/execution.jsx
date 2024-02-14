27ed4f58de0c0e0ef6aa19fd58b6462facd1895e
HAITAME <haitame.laframe@etu.uae.ac.ma>
2024-02-14 21:32:43 +0100

    Refactor Execution component layout

--- src/components/execution/execution.jsx
+++ src/components/execution/execution.jsx
@@ -22,51 +22,45 @@ import {
 function Execution() {
   return (
     <>
-    
-    
-      <div className="bg-white shadow-lg rounded ashraf">
-                  <div className="flex flex-col h-full items-center justify-center p-6">
-                    <div className="">
-                      <span className="flex font-semibold text-xl">Plan d'execution</span>
-
-                      </div>                       
-                      <Separator className="pb-1 "/>
-
-                    <div className= "w-full flex">
-                      <Table >
-                        <TableHeader>
-                          <TableRow>
-                            <TableHead>bytes</TableHead>
-                            <TableHead>cost</TableHead>
-                            <TableHead>rows</TableHead>
-                            <TableHead>time</TableHead>
-                            <TableHead>name</TableHead>
-                            <TableHead>operation</TableHead>
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          <TableRow>
-                            <TableCell>600</TableCell>
-                            <TableCell>2(0)</TableCell>
-                            <TableCell>15</TableCell>
-                            <TableCell>00:00:07</TableCell>
-                            <TableCell>Etudiant</TableCell>
-                            <TableCell>SELECT * FROM Etudiant</TableCell>
-                          </TableRow>
-                          <TableRow>
-                            <TableCell>600</TableCell>
-                            <TableCell>2(0)</TableCell>
-                            <TableCell>15</TableCell>
-                            <TableCell>00:00:07</TableCell>
-                            <TableCell>Etudiant</TableCell>
-                            <TableCell>SELECT * FROM Etudiant</TableCell>
-                          </TableRow>
-                        </TableBody>
-                      </Table>
-                    </div>
-                  </div>
-
-
+      <div className="bg-white shadow-lg rounded ashraf h-auto ">
+        <div className="flex flex-col  items-center justify-center p-6">
+          <div className="">
+            <span className="flex font-semibold text-xl">Plan d'execution</span>
+          </div>
+          <Separator className="pb-1 "/>
+          <div className= "w-full flex ">
+            <Table >
+              <TableHeader>
+                <TableRow>
+                  <TableHead>bytes</TableHead>
+                  <TableHead>cost</TableHead>
+                  <TableHead>rows</TableHead>
+                  <TableHead>time</TableHead>
+                  <TableHead>name</TableHead>
+                  <TableHead>operation</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>600</TableCell>
+                  <TableCell>2(0)</TableCell>
+                  <TableCell>15</TableCell>
+                  <TableCell>00:00:07</TableCell>
+                  <TableCell>Etudiant</TableCell>
+                  <TableCell>SELECT * FROM Etudiant</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>600</TableCell>
+                  <TableCell>2(0)</TableCell>
+                  <TableCell>15</TableCell>
+                  <TableCell>00:00:07</TableCell>
+                  <TableCell>Etudiant</TableCell>
+                  <TableCell>SELECT * FROM Etudiant</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
       </div>
     </>
   )
